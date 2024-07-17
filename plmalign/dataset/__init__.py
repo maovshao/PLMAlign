09ea3452d49740e600a1d530f360e465df3b9984
maovshao <maovshao@gmail.com>
2024-07-17 16:02:34 +0800

    2024.7.17: Normal Fix

---
+++ plmalign/dataset/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2020-- deepblast development team
+#
+# Files in this folder are modified from [DeepBLAST: https://www.nature.com/articles/s41587-023-01917-2].
+#
+# This source code is licensed under the BSD 3-Clause "New" or "Revised" License found in the
+# LICENSE file in the root directory of this source tree.
