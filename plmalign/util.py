6b390257b6bb204977a17cf4fb8b334652d59979
maovshao <maovshao@gmail.com>
2024-07-27 01:30:54 +0800

    2024.7.27: LICENSE UPDATE

--- plmalign/util.py
+++ plmalign/util.py
@@ -27,8 +27,8 @@ def make_parent_dir(path):
 #
 # This file is modified from [https://github.com/labstructbioinf/pLM-BLAST].
 #
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions.
+
 @numba.njit('f4[:,:](f4[:,:], f4[:,:])', nogil=True, fastmath=True, cache=True)
 def dot_product(X: np.ndarray, Y: np.ndarray) -> np.ndarray:
 	assert X.ndim == 2 and Y.ndim == 2
