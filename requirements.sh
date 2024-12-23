4ceb8f23315b5adf094f7285f45076d2d2407753
maovshao <maovshao@gmail.com>
2024-12-23 14:50:09 -0800

    2024.12.23: requirement.sh update

--- requirements.sh
+++ requirements.sh
@@ -6,14 +6,13 @@ conda activate plmalign
 conda install -c conda-forge biopython
 conda install -c conda-forge tqdm
 conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.1 -c pytorch
-#(Option for CPU only) pip install torch==1.7.1+cpu torchvision==0.8.2+cpu torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+# Or other pytorch versions depending on your local environment
 conda install -c conda-forge pandas
 conda install -c conda-forge seaborn
 conda install -c conda-forge scikit-learn
 conda install ipykernel --update-deps --force-reinstall
 conda install dask -c conda-forge
 conda install matplotlib -c conda-forge 
-conda install seaborn -c conda-forge
 
 pip install scipy
 pip install numba
