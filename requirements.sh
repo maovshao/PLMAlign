2a4b13a2208c379c592619832b4de4bab7888acd
maovshao <maovshao@gmail.com>
2025-09-08 22:45:05 -0700

    2025.9.8: NORMAL UPDATE

--- requirements.sh
+++ requirements.sh
@@ -5,8 +5,6 @@ conda activate plmalign
 #conda environment
 conda install -c conda-forge biopython
 conda install -c conda-forge tqdm
-conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.1 -c pytorch
-# Or other pytorch versions depending on your local environment
 conda install -c conda-forge pandas
 conda install -c conda-forge seaborn
 conda install -c conda-forge scikit-learn
@@ -14,6 +12,9 @@ conda install ipykernel --update-deps --force-reinstall
 conda install dask -c conda-forge
 conda install matplotlib -c conda-forge 
 
+pip install torch torchvision
+# Or other pytorch versions depending on your local environment
+pip install numpy==1.26
 pip install scipy
 pip install numba
 pip install transformers
